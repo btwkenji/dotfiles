c3296ae8a02bf3702e1cc9912652ed38473338c2
nezutero <nezutero@gmail.com>
2024-12-18 20:02:08 +0100

    chore(config/hyprland): remove lines that dont't work anymore

--- install.sh
+++ install.sh
@@ -6,9 +6,8 @@ sudo pacman -S --noconfirm \
     zathura dunst btop waybar swaybg alacritty ranger rofi \
     telegram-desktop wl-clipboard grim slurp npm pnpm yarn gimp qt5ct lxappearance \
     fastfetch zsh materia-gtk-theme pavucontrol zip unzip tree \
-    xf86-video-intel networkmanager feh noto-fonts-cjk zig \
-    python nodejs ttf-dejavu noto-fonts-emoji zathura-pdf-mupdf fzf docker docker-compose \
-    alsa-utils dnsutils
+    feh noto-fonts-cjk zig python nodejs ttf-dejavu noto-fonts-emoji \
+    zathura-pdf-mupdf fzf docker docker-compose alsa-utils dnsutils
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -16,7 +15,7 @@ makepkg -si
 cd ..
 rm -rf yay
 
-mkdir -p ~/dev/mantain
+mkdir -p ~/dev
 mkdir -p ~/docs/books
 mkdir -p ~/pics/{walls,screenshots}
 mkdir -p ~/vids/screencaptures
@@ -31,14 +30,16 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 yay -S ttf-cascadia-code-nerd
 
-yay -S spotify video-downloader vesktop wlogout spicefy-cli
+yay -S spotify video-downloader vesktop wlogout 
 
-git clone --depth=1 https://github.com/spicetify/spicetify-themes.git 
-cd spicetify-themes
-cp -r * ~/.config/spicetify/Themes
-spicetify config current_theme Sleek
-spicetify config color_scheme UltraBlack
-spicetify config custom_apps new-releases
-spicetify config custom_apps lyrics-plus
-spicetify config custom_apps lyrics-plus
-spicetify apply
+# spicefy-cli
+
+# git clone --depth=1 https://github.com/spicetify/spicetify-themes.git 
+# cd spicetify-themes
+# cp -r * ~/.config/spicetify/Themes
+# spicetify config current_theme Sleek
+# spicetify config color_scheme UltraBlack
+# spicetify config custom_apps new-releases
+# spicetify config custom_apps lyrics-plus
+# spicetify config custom_apps lyrics-plus
+# spicetify apply
