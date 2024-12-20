1f27ec38f09159b1a3070e87ce84eebe08bed84b
nezutero <nezutero@gmail.com>
2024-12-20 23:41:58 +0100

    chore: extend packages list, remove unused commented stuff

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 sudo pacman -Syu
 
 sudo pacman -S --noconfirm \
-    zathura dunst btop waybar swaybg alacritty ranger rofi \
-    telegram-desktop wl-clipboard grim slurp npm pnpm yarn gimp qt5ct lxappearance \
-    fastfetch zsh materia-gtk-theme pavucontrol zip unzip tree \
-    feh noto-fonts-cjk zig python nodejs ttf-dejavu noto-fonts-emoji \
-    zathura-pdf-mupdf fzf docker docker-compose alsa-utils dnsutils
+    zathura dunst btop waybar swaybg alacritty ranger rofi firefox-developer-edition chromium \
+    telegram-desktop wl-clipboard grim slurp npm pnpm yarn gimp qt5ct lxappearance foliate \
+    fastfetch zsh materia-gtk-theme pavucontrol zip unzip tree obs-studio audacity steam \
+    feh noto-fonts-cjk zig python nodejs ttf-dejavu noto-fonts-emoji kdenlive sof-firmware \
+    zathura-pdf-mupdf fzf docker docker-compose alsa-utils dnsutils distrobox bluez bluez-utils
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -16,9 +16,9 @@ cd ..
 rm -rf yay
 
 mkdir -p ~/dev
-mkdir -p ~/docs/books
-mkdir -p ~/pics/{walls,screenshots}
-mkdir -p ~/vids/screencaptures
+mkdir -p ~/Documents/{Docs,Books}
+mkdir -p ~/Pictures/{Walls,Screenshots}
+mkdir -p ~/Videos/Screencaptures
 mkdir -p ~/.local/bin
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -31,15 +31,3 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 yay -S ttf-cascadia-code-nerd
 
 yay -S spotify video-downloader vesktop wlogout 
-
-# spicefy-cli
-
-# git clone --depth=1 https://github.com/spicetify/spicetify-themes.git 
-# cd spicetify-themes
-# cp -r * ~/.config/spicetify/Themes
-# spicetify config current_theme Sleek
-# spicetify config color_scheme UltraBlack
-# spicetify config custom_apps new-releases
-# spicetify config custom_apps lyrics-plus
-# spicetify config custom_apps lyrics-plus
-# spicetify apply
